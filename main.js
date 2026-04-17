1ee1f793b53698101b046407c99825a33cdd2e8a
TanushkaNadeeshan <sadtnadeeshan@gmail.com>
2026-04-17 19:07:43 +1000

    4.6

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 const svg = d3.select(".responsive-svg-container")
   .append("svg")
-  .attr("viewBox", "0 0 1200 1600")
+  .attr("viewBox", "0 0 500 600")
   .style("border", "1px solid black");
 
 const drawBarChart = data => {
-  const barHeight = 20;
-  const barSpacing = 8;
+  const xScale = d3.scaleLinear()
+    .domain([0, 1200])
+    .range([0, 400]);
+
+  const yScale = d3.scaleBand()
+    .domain(data.map(d => d.brand))
+    .range([0, 600])
+    .padding(0.1);
 
   svg.selectAll("rect")
     .data(data)
     .join("rect")
     .attr("class", d => `bar bar-${d.count}`)
     .attr("x", 0)
-    .attr("y", (d, i) => i * (barHeight + barSpacing))
-    .attr("width", d => d.count)
-    .attr("height", barHeight)
+    .attr("y", d => yScale(d.brand))
+    .attr("width", d => xScale(d.count))
+    .attr("height", yScale.bandwidth())
     .attr("fill", "blue");
 };
 
