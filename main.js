a1d8fc51d29e34bb447547c6018ddae64c8bdb44
TanushkaNadeeshan <sadtnadeeshan@gmail.com>
2026-04-17 18:39:36 +1000

    4.5

--- main.js
+++ main.js
@@ -3,9 +3,20 @@ const svg = d3.select(".responsive-svg-container")
   .attr("viewBox", "0 0 1200 1600")
   .style("border", "1px solid black");
 
-function drawBarChart(data) {
-  console.log("drawBarChart called with data:", data);
-}
+const drawBarChart = data => {
+  const barHeight = 20;
+  const barSpacing = 8;
+
+  svg.selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("class", d => `bar bar-${d.count}`)
+    .attr("x", 0)
+    .attr("y", (d, i) => i * (barHeight + barSpacing))
+    .attr("width", d => d.count)
+    .attr("height", barHeight)
+    .attr("fill", "blue");
+};
 
 d3.csv("./data/tvBrandCount.csv", d => {
   return {
@@ -13,24 +24,9 @@ d3.csv("./data/tvBrandCount.csv", d => {
     count: +d.count
   };
 }).then(data => {
-  console.log(data);
-  console.log(data.length);
-  console.log(d3.max(data, d => d.count));
-  console.log(d3.min(data, d => d.count));
-  console.log(d3.extent(data, d => d.count));
-
   data.sort((a, b) => b.count - a.count);
   console.log(data);
-
   drawBarChart(data);
 }).catch(error => {
-  console.error("Error loading the CSV file:", error);
+  console.error("Error loading CSV:", error);
 });
-
-svg
-  .append("rect")
-  .attr("x", 10)
-  .attr("y", 10)
-  .attr("width", 414)
-  .attr("height", 16)
-  .attr("fill", "blue");
