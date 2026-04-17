0ec1cfc7df11c6d588f6dac9bb0a6abb6fdbf9ed
TanushkaNadeeshan <sadtnadeeshan@gmail.com>
2026-04-17 18:35:48 +1000

    4.4

--- main.js
+++ main.js
@@ -3,6 +3,30 @@ const svg = d3.select(".responsive-svg-container")
   .attr("viewBox", "0 0 1200 1600")
   .style("border", "1px solid black");
 
+function drawBarChart(data) {
+  console.log("drawBarChart called with data:", data);
+}
+
+d3.csv("./data/tvBrandCount.csv", d => {
+  return {
+    brand: d.brand,
+    count: +d.count
+  };
+}).then(data => {
+  console.log(data);
+  console.log(data.length);
+  console.log(d3.max(data, d => d.count));
+  console.log(d3.min(data, d => d.count));
+  console.log(d3.extent(data, d => d.count));
+
+  data.sort((a, b) => b.count - a.count);
+  console.log(data);
+
+  drawBarChart(data);
+}).catch(error => {
+  console.error("Error loading the CSV file:", error);
+});
+
 svg
   .append("rect")
   .attr("x", 10)
